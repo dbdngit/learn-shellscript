6eaaa4138866409f5dbc76e756d30dc052485d99
dbdngit <56876141+dbdngit@users.noreply.github.com>
2020-09-28 08:18:58 +0100

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -5,3 +5,5 @@ echo "I am in to release pipeline"
 echo "\n\nI am testing from jenkins.. Hook services"
 echo "Poll scm works "
 echo "still it didn't triggerd"
+echo "my machine IP is :"
+ifconfig  | grep -A1 eth0 | grep inet

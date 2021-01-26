f9c865a7daa16625aa60979010daf24cab1ee0be
dbdngit <56876141+dbdngit@users.noreply.github.com>
2021-01-26 07:55:15 +0000

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,13 +1,2 @@
-echo "trigged via webhook"
-echo "Hello World!!!!!!"
-echo "This is for auto trigger test"
-date
-echo "I am in to release pipeline"
-echo "\n\nI am testing from jenkins.. Hook services"
-echo "Poll scm works "
-echo "still it didn't triggerd"
-echo "my machine IP is :"
-ifconfig  | grep -A1 eth0 | grep inet
-sleep 5
 echo "we are done with build"
 echo "new change after webhook implementation  "
